bd530edc5111878bfaf509542a6984c5f18b08b3
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-05-26 14:45:25 -0300

    :lock: Implementação da Spring Security

---
+++ src/main/java/com/generation/lojagames/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.generation.lojagames.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.lojagames.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	Optional<Usuario> findByUsuario(String usuario);
+	
+	/* SELECT * FROM tb_usuarios WHERE usuario = ? */
+}
